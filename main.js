268afedde62d9655c78c12fcb3aa589da797631d
Mark Landeryou <samurai96@hotmail.com>
2022-08-29 18:25:34 -0400

    feat: working on the getFullYear function

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 //import './style.css';
-import { copyright } from './js/copyright';
+//import { copyright } from './js/copyright.js';
 
 console.log('Hello World');
+
+// function copyright() {
+//   console.log('Copyright has loaded');
+// }
+console.log('Copyright Date has loaded');
+const copyrightDate = new Date();
+let year = copyrightDate.getFullYear();
+console.log(year);
